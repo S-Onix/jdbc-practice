95cdf02caca66c28c17eafa682982a95ab51abb5
S-Onix <msyoo101@hansung.ac.kr>
2023-02-20 22:01:07 +0900

    jdbc prcatice

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+    @BeforeEach
+    void setUp(){
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionMananger.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+        userDao.create(new User("wizard", "123", "name", "email"));
+        User user = userDao.findByUserId("hone");
+        Assertions.assertThat(user).isEqualTo(new User("wizard", "123", "name", "email"));
+    }
+}
